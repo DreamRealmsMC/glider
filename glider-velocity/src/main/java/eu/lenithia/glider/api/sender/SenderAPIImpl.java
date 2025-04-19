6a423ef0941c3fa85bbd8df23501099669549155
len <137_len@proton.me>
2025-04-20 00:38:49 +0200

    Sender API impelemented

--- glider-velocity/src/main/java/eu/lenithia/glider/api/sender/SenderAPIImpl.java
+++ glider-velocity/src/main/java/eu/lenithia/glider/api/sender/SenderAPIImpl.java
@@ -5,8 +5,11 @@ import eu.lenithia.glider.GliderVelocity;
 import eu.lenithia.glider.clusters.system.GCluster;
 import eu.lenithia.glider.clusters.system.GGroup;
 import eu.lenithia.glider.clusters.system.GServer;
+import eu.lenithia.glider.sender.Sender;
 import eu.lenithia.glider.sender.events.SkipAbleSenderEvent;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SenderAPIImpl implements SenderAPI {
@@ -19,46 +22,46 @@ public class SenderAPIImpl implements SenderAPI {
 
     @Override
     public void send(GCluster cluster, Player player) {
-
+        new Sender(cluster, Collections.emptyList(), player, "API", 0);
     }
 
     @Override
     public void send(GCluster cluster, Player player, List<SkipAbleSenderEvent> skipEvents) {
-
+        new Sender(cluster, skipEvents, player, "API", 0);
     }
 
     @Override
     public void send(GCluster cluster, Player player, List<SkipAbleSenderEvent> skipEvents, String sender, int tryCount) {
-
+        new Sender(cluster, skipEvents, player, sender, tryCount);
     }
 
     @Override
     public void send(GGroup group, Player player) {
-
+        new Sender(group, Collections.emptyList(), player, "API", 0);
     }
 
     @Override
     public void send(GGroup group, Player player, List<SkipAbleSenderEvent> skipEvents) {
-
+        new Sender(group, skipEvents, player, "API", 0);
     }
 
     @Override
     public void send(GGroup group, Player player, List<SkipAbleSenderEvent> skipEvents, String sender, int tryCount) {
-
+        new Sender(group, skipEvents, player, sender, tryCount);
     }
 
     @Override
     public void send(GServer server, Player player) {
-
+        new Sender(server, Collections.emptyList(), player, "API", 0);
     }
 
     @Override
     public void send(GServer server, Player player, List<SkipAbleSenderEvent> skipEvents) {
-
+        new Sender(server, skipEvents, player, "API", 0);
     }
 
     @Override
     public void send(GServer server, Player player, List<SkipAbleSenderEvent> skipEvents, String sender, int tryCount) {
-
+        new Sender(server, skipEvents, player, sender, tryCount);
     }
 }
